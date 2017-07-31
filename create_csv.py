0579620dcc748cef39a40510134d93f1fdd9b304
Ryan McCormick <rmccorm4@binghamton.edu>
2017-07-31 16:42:20 -0400

    update table

--- create_csv.py
+++ create_csv.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	start = int(input('Enter network number to start at: '))
 	end = int(input('Enter network number to end at: '))
 	
-	data = [['set', 'classes', 'accuracy']]
+	data = [['SET', 'CLASSES', 'ACCURACY']]
 	for i in range(start, end+1):
 		set_path = 'random/' + str(i)
 		words10_path = 'sets/' + set_path + '/words10.txt'
@@ -18,18 +18,15 @@ if __name__ == '__main__':
 			classes = [x.split('\t')[1].split(',')[0].strip() for x in classes]
 		
 		classes = str(classes).replace(',', '')
-
 		model_path = 'work/training/tiny_imagenet/sets/' + set_path + '/best_weights_val_loss.hdf5'
-
 		accuracy = '%.2f%%' % float(train_tiny_imagenet(wnids=set_path, resize=True, load=model_path))
 		
-		print(classes)
 		line = set_path + ',' + classes + ',' + accuracy
 		data.append(line.split(','))
-	print(data)
 
 	with open('table.csv', 'w') as csv_file:
 		writer = csv.writer(csv_file, delimiter=',')
 		for line in data:
 			writer.writerow(line)
+
 	csv_file.close()
